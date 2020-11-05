35ef15b3a942994cc1d5024fa01958109fbbaf7f
nishant <nishant.singh87@gmail.com>
2020-11-06 00:43:48 +0530

    Dockerize server.

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
@@ -15,20 +12,35 @@ namespace GrpcServer
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.ConfigureKestrel(options =>
-                        {
-                            // Setup a HTTP/2 endpoint without TLS.
-                            options.ListenLocalhost(5000, o => o.Protocols = 
+        private  static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                            .ConfigureWebHostDefaults(getConfiguration());
+        }
+
+
+        private static Action<IWebHostBuilder> configureForMacos = webBuilder =>
+        {
+            webBuilder.ConfigureKestrel(options =>
+            {
+                // Setup a HTTP/2 endpoint without TLS.
+                options.ListenLocalhost(5000, o => o.Protocols =
                                 HttpProtocols.Http2);
-                        });
-                        webBuilder.UseStartup<Startup>();
-                    });
+            });
+            webBuilder.UseStartup<Startup>();
+        };
+
+        private static Action<IWebHostBuilder> conigure = webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        };
+        
+        private static Action<IWebHostBuilder> getConfiguration()
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? configureForMacos : conigure;
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,20 @@
+https://github.com/dotnetgik/gRPCDocker/tree/master/Properties
+
+
+
+Build docker images
+
+```
+cd GrpcServer
+docker build -t grpc-server .
+
+cd ../GrpcClientConsole/
+docker build -t grpc-client .
+```
+
+Run server
+
+```
+docker run -p 5000:80 grpc-server
+```
+
